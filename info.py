5feb2939fd42e3e323994f8462002be5691d0075
TLKTinku <tlktinkubro@gmail.com>
2026-07-31 16:54:59 +0530

    Add files via upload

--- info.py
+++ info.py
@@ -121,6 +121,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # Channel & Group Links Configuration
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/officialmymovies') # Group link for the bot
+BACKUP_CHANNEL_LINK = environ.get('BACKUP_CHANNEL_LINK', GRP_LNK) # Backup channel link, used for the clickable Name in file captions
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/TLK_Tinku_bro') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/moviesinhindigroup') # Update channel link for the bot
 
